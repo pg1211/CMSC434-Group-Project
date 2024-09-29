5bc03c872622d1e5a98541668eb1a28b518256fb
mystic4kt <146299263+mystic4kt@users.noreply.github.com>
2024-09-29 03:03:01 -0400

    Update profile-script.js

--- profile-script.js
+++ profile-script.js
@@ -5,14 +5,3 @@ function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
-
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsById("myPopup");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}

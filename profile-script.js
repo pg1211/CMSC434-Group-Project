9efd7d8d001dc7cde39c2a6617a0ca1354c2457e
mystic4kt <146299263+mystic4kt@users.noreply.github.com>
2024-09-29 02:25:59 -0400

    Update profile-script.js

--- profile-script.js
+++ profile-script.js
@@ -1,1 +1,9 @@
+//the following code is based on a sample from https://www.w3schools.com/howto/howto_js_popup.asp
 
+<script>
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
